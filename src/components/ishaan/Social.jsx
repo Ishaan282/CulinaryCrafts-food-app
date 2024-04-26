ac803c0f96cbd14e27769c40509f55c7940a75ef
Ishaan1106 <shunsuke062004@gmail.com>
2024-04-27 04:10:26 +0530

    some progress (ishaan)

--- src/components/ishaan/Social.jsx
+++ src/components/ishaan/Social.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import './Social.css';
-//importing css module
 import s from './Social.module.css';
+
+//importing icons
+import gif_icon from './icons/gif.svg'
+import upload_icon from './icons/upload.svg'
 
 function Social() {
   return (
-    <div id={s.main}></div>
+    <div id={s.main}>
+      <div id={s.bottom}>
+      <img src={upload_icon} alt="upload_icon" className={` ${s.bottom_icon}`}/>
+        <img src={gif_icon} alt="gif_icon" className={`${s.bottom_icon}`}/>
+      </div>
+    </div>
   )
 }
 

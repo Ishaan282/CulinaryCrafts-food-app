0c88d3708645c23746d0ae37e20c7c040e94d25d
Samiksha <singhsamikshasingh2004@gmail.com>
2024-04-28 04:22:37 +0530

    i think main is done

--- src/components/samiksha/Home.js
+++ src/components/samiksha/Home.js
@@ -13,8 +13,17 @@ import salad from'./salads.jpg';
 import soup from './soups.jpg';
 import sweet from './desserts.jpg';
 import vege from './vegetarian.jpg';
-import pancake from './pancakes11.jpg'
-import salads from './salads.jpg'
+import pancake from './pancakes11.jpg';
+import salads from './salads.jpg';
+import snack1 from './snack1.jpg';
+import snack2 from './snack2.jpg';
+import snack3 from './snack3.jpg';
+import snack4 from './snack4.jpg';
+import snack5 from './snack5.jpg';
+import dish1 from './dish1.jpg';
+import dish2 from './dish2.jpg';
+import main from './mainmain.jpg';
+
 
 const Home = () => {
   return (
@@ -131,7 +140,9 @@ const Home = () => {
     <div className={a.latestRecipesSection}>
       <h2 className={a.heading}>Latest Recipes</h2>
       <div className={a.recipeContainer}>
+      
         <div className={a.recipeBox}>
+        <Link to="/recipes" className={a.link}>
           <img src={pasta} alt="Recipe 1" className={a.recipeImage} />
           <p className={a.dessert}>Pastas</p>
           <p className={a.name}>Lemon Pasta</p>
@@ -143,8 +154,10 @@ const Home = () => {
             <span className={a.starEmpty}></span>
             <span className={a.ratingNumber}>54</span>
           </div>
+          </Link>
         </div>
         <div className={a.recipeBox}>
+        <Link to="/recipes" className={a.link}>
           <img src={pancake} alt="Recipe 2" className={a.recipeImage} />
           <p className={a.dessert}>Dessert</p>
           <p className={a.name}>Blueberry Pancakes</p>
@@ -156,9 +169,11 @@ const Home = () => {
             <span className={a.star}></span>
             <span className={a.ratingNumber}>32</span>
           </div>
+          </Link>
         </div>
 
         <div className={a.recipeBox}>
+        <Link to="/recipes" className={a.link}>
           <img src={salads} alt="Recipe 2" className={a.recipeImage} />
           <p className={a.dessert}>Salads</p>
           <p className={a.name}>Mixed Pasta</p>
@@ -170,10 +185,12 @@ const Home = () => {
             <span className={a.starEmpty}></span>
             <span className={a.ratingNumber}>21</span>
           </div>
+          </Link>
         </div>
 
 
         <div className={a.recipeBox}>
+        <Link to="/recipes" className={a.link}>
           <img src={sweet} alt="Recipe 3" className={a.recipeImage} />
           <p className={a.dessert}>Dessert</p>
           <p className={a.name}>Cosmic Brownies</p>
@@ -185,12 +202,96 @@ const Home = () => {
             <span className={a.starEmpty}></span>
             <span className={a.ratingNumber}>51</span>
           </div>
+          </Link>
         </div>
       </div>
       <Link to="/recipes" className={a.link}>
       <button className={a.loadMoreBtn}>View More</button>
       </Link>
     </div>
+
+
+    <div className={a.todayCookSection}>
+  <h2 className={a.sectionTitle}>So, what will you cook today?</h2>
+
+  <div className={a.cookContainer}>
+    {/* First section */}
+    
+    <div className={a.section}>
+    <Link to="/recipes" className={a.link}>
+      <img src={dish1} alt="Most searched dish" />
+      <p className={a.paracolored}>Recipes</p>
+      <p className={a.paraidk}>Check out the most searched dish of today</p>
+      </Link>
+      <Link to="/recipes" className={a.link}>
+      <img src={dish2} alt="New recipes" />
+      <p className={a.paracolored}>Recipes</p>
+      <p className={a.paraidk}>New recipes just for you</p>
+      </Link>
+    </div>
+    {/* Second section */}
+    
+    <div className={a.section}>
+    <Link to="/recipes" className={a.link}>
+      <img src={main} alt="Egg salad" />
+      <p>Veggie Salad</p>
+      <div className={a.stars}>★★★★☆</div>
+    </Link>
+    </div>
+
+    {/* Third section */}
+    <div className={a.section}>
+    <Link to="/recipes" className={a.link}>
+      {[snack1, snack2, snack3, snack4, snack5].map((dish, index) => (
+        <div key={index} className={a.smallDish}>
+          <div className={a.text}>
+            <p>Surprise Snack Type {index + 1}</p>
+            <div className={a.stars}>★★★★★</div>
+          </div>
+          <img src={dish} alt={`Marmalade Type ${index + 1}`} />
+        </div>
+      ))}
+      </Link>
+    </div>
+  </div>
+</div>
+
+    <div className={a.footer}>
+      <div className={a.footerContainer}>
+        <div className={a.footerSection}>
+          <h3>Quick Links</h3>
+          <Link to="/home">Home</Link>
+          <Link to="/recipes">Recipes</Link>
+          <Link to="/cart">Cart</Link>
+          <Link to="/social">Social</Link>
+        </div>
+        <div className={a.footerSection}>
+          <h3>Follow Us</h3>
+          <Link to="http://instagram.com"><i className={`fa fa-instagram ${a.socialIcon}`}></i></Link>
+          <Link to="http://facebook.com"><i className={`fa fa-facebook ${a.socialIcon}`}></i></Link>
+          <Link to="http://twitter.com"><i className={`fa fa-twitter ${a.socialIcon}`}></i></Link>
+          <Link to="http://pinterest.com"><i className={`fa fa-pinterest ${a.socialIcon}`}></i></Link>
+        </div>
+        <div className={a.footerSection}>
+          <h3>Contact Us</h3>
+          <p>email@example.com</p>
+          <p>+123 456 7890</p>
+        </div>
+        <div className={a.footerSection}>
+          <h3>Subscribe</h3>
+          <input type="email" placeholder="Your Email" />
+          <button type="submit">Subscribe</button>
+        </div>
+      </div>
+      <div className={a.copyRight}>
+        © 2024 Team-4 G22. All rights reserved.
+      </div>
+      
+    </div>
+   
+
+
+
     
     </>
   );

6c758d8d92a3b8f2b8a445dc97b695732ddf2667
Ishaan1106 <shunsuke062004@gmail.com>
2024-04-28 18:27:35 +0530

    ishaan

--- src/components/ishaan/Social.jsx
+++ src/components/ishaan/Social.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import s from './Social.module.css';
+import Message from './componenets/Message.jsx';
+import Profile from './componenets/Profile.jsx';
 
 //importing icons
 import gif_icon from './icons/gif.png';
@@ -7,6 +9,9 @@ import upload_icon from './icons/upload.png';
 
 //importing profile pictures
 import pfp1 from './pfp/pfp1.jpg';
+import pfp2 from './pfp/pfp2.jpg';
+
+import test from './images/test.jpg';
 
 function Social() {
     const emojis = ['😀', '😃', '😄', '😁', '😆', '😅', '😂', '🤣', '😊', '😇', '🙂', '🙃', '😉', '😌', '😍', '🥰', '😘', '😗', '😙', '😚', '😋', '😛', '😝', '😜', '🤪', '🤨', '🧐', '🤓', '😎', '🤩', '🥳', '😏', '😒', '😞', '😔', '😟', '😕', '🙁', '☹️', '😣', '😖', '😫', '😩', '🥺', '😢', '😭', '😤', '😠', '😡', '🤬', '🤯', '😳', '🥵', '🥶', '😱', '😨', '😰', '😥', '😓', '🤗', '🤔', '🤭', '🤫', '🤥', '😶', '😐', '😑', '😬', '🙄', '😯', '😦', '😧', '😮', '😲', '🥱', '😴', '🤤', '😪', '😵', '🤐', '🥴', '🤢', '🤮', '🤧', '😷', '🤒', '🤕', '🤑', '🤠', '😈', '👿', '👹', '👺', '🤡', '👻', '💀', '☠️'];
@@ -22,77 +27,66 @@ function Social() {
         setCurrentEmoji(getRandomEmoji());
     };
 
-    function Profile({ id, imgSrc, name, containerClass, imgClass, nameClass }) {
-        return (
-            <div id={id} className={containerClass}>
-                <img src={imgSrc} alt="profile" className={imgClass}/>
-                <p className={nameClass}>{name}</p>
-            </div>
-        );
-    }
-
-    // Define Message component here
-    function Message({ id, imgSrc, name, message, containerClass, imgClass, nameClass, messageClass }) {
-        return (
-            <div id={id} className={containerClass}>
-                <img src={imgSrc} alt="profile" className={imgClass}/>
-                <div id={s.text}>
-                    <p className={nameClass}>{name}</p>
-                    <p className={messageClass}>{message}</p>
-                </div>
-            </div>
-        );
-    }
-
     const [messages, setMessages] = useState([]);
 
-    const handleKeyPress = (event) => {
-        if (event.key === 'Enter') {
-            event.preventDefault(); // Prevent form submission
-            const textInput = event.target.value.trim();
-            if (textInput !== '') {
-                setMessages(prevMessages => [...prevMessages, { id: s.message1, imgSrc: pfp1, name: "Shunsuke", message: textInput, containerClass: s.texts, imgClass: s.profile_picture, nameClass: s.profile_name, messageClass: s.msg1 }]);
-                event.target.value = '';
-            }
-        }
+    const handleInput = (event) => {
+        event.target.style.height = 'auto';
+        event.target.style.height = (event.target.scrollHeight) + 'px';
     };
 
-    return (
-        <div id={s.main}>
-            {/* sidebar */}
-            <div className={`${s.main1}`}>
-                <header id={`${s.head}`}>
-                    People
-                </header>
-                <hr style={{border: 'none', borderTop: '2px solid black', width: '103%',margin: `0 -3%`}} />
+const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+        event.preventDefault(); // Prevent form submission
+        const textInput = event.target.value.trim();
+        if (event.shiftKey) {
+            // If the Shift key is being held down, insert a line break
+            event.target.value += '\n';
+        } else if (textInput !== '') {
+            // If the Shift key is not being held down, send the message
+            setMessages(prevMessages => [...prevMessages, { id: s.message1, imgSrc: pfp2, name: "Bread", message: textInput, containerClass: s.texts, messageClass: s.msg1 }]);
+            event.target.value = '';
+        }
+    }
+};
 
-                <div id={s.profiles}>
-                    <Profile id={s.pr1} imgSrc={pfp1} name="Shunsuke" containerClass={s.profile} imgClass={s.profile_picture} nameClass={s.profile_name} />
-                    <Profile id={s.pr2} imgSrc={pfp1} name="Shunsuke" containerClass={s.profile} imgClass={s.profile_picture} nameClass={s.profile_name} />
-                </div>
+return (
+    <div id={s.main}>
+        {/* sidebar */}
+        <div className={`${s.main1}`}>
+            <header id={`${s.head}`}>
+                People
+            </header>
+            <hr style={{border: 'none', borderTop: '2px solid black', width: '103%',margin: `0 -3%`}} />
+
+            <div id={s.profiles}>
+                <Profile id={s.pr1} imgSrc={pfp1} name="Shunsuke" />
+                <Profile id={s.pr2} imgSrc={pfp2} name="Bread" />
+                <Profile id={s.pr3} imgSrc={pfp1} name="Shunsuke" />
+            </div>
+        </div>
+
+        {/* chatting area  */}
+        <div className={`${s.main2}`}>
+            <div className={`${s.chat}`}>
+                <Message id={s.message1} imgSrc={pfp1} name="Shunsuke" message="Hey, how are you?" containerClass={s.texts} messageClass={s.msg1} photo={test}/>
+                
+                {messages.map((message, index) => (
+                    <Message key={index} {...message} />
+                ))}
             </div>
 
-            {/* chatting area  */}
-            <div className={`${s.main2}`}>
-                <div className={`${s.chat}`}>
-				<Message id={s.message1} imgSrc={pfp1} name="Shunsuke" message="Hey, how are you?" containerClass={s.texts} imgClass={s.profile_picture} nameClass={s.profile_name} messageClass={s.msg1} />
-                    {messages.map((message, index) => (
-                        <Message key={index} {...message} />
-                    ))}
-                </div>
-
-            {/* textingbar */}
-                <div id={s.bottom}>
-                    <img src={upload_icon} alt="upload_icon" className={` ${s.bottom_icon}`}/>
-                    <input id={s.enter_text} placeholder="text here" onKeyPress={handleKeyPress} />
-                    <div className={`${s.right}`}>
-                        <img src={gif_icon} alt="gif_icon" className={`${s.bottom_icon}`}/>
-                        <div className={`${s.bottom_icon} ${s.emoji}`} onMouseEnter={changeEmoji}>{currentEmoji}</div>
-                    </div>
+        {/* textingbar */}
+            <div id={s.bottom}>
+                <img src={upload_icon} alt="upload_icon" className={` ${s.bottom_icon}`}/>
+                <textarea id={s.enter_text} placeholder="text here" onKeyPress={handleKeyPress} onInput={handleInput} />
+                <div className={`${s.right}`}>
+                    <img src={gif_icon} alt="gif_icon" className={`${s.bottom_icon}`}/>
+                    <div className={`${s.bottom_icon} ${s.emoji}`} onMouseEnter={changeEmoji}>{currentEmoji}</div>
                 </div>
             </div>
         </div>
-    );
+    </div>
+);
 }
 
 export default Social;
---
+++ src/components/ishaan/componenets/Message.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import s from '../Social.module.css';
+
+function Message({ id, imgSrc, name, message, messageClass, photo }) {
+    const formattedMessage = message.split('\n').map((line, index) => (
+        <React.Fragment key={index}>
+            {line}
+            <br />
+        </React.Fragment>
+    ));
+
+    return (
+        <div id={id} className={s.profile2}>
+            <img src={imgSrc} alt="profile" className={s.profile_picture}/>
+            <div id={s.text}>
+                <p className={s.profile_name}>{name}</p>
+                <p className={messageClass} style={{marginBottom:`-10px`}}>{formattedMessage}</p>
+                {photo && <img src={photo} alt="photo" className={s.photo} />}
+            </div>
+        </div>
+    );
+}
+
+export default Message;

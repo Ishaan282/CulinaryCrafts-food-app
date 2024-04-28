b8484f1e20138d6ea96d845eae5a2e7e40a60ba9
Ishaan1106 <shunsuke062004@gmail.com>
2024-04-29 02:36:29 +0530

    ishaan

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
     <BrowserRouter>
     <Navbar />
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/Home" element={<Home />} />
           <Route path="/Cart" element={<Cart />} />
           <Route path="/Social" element={<Social />} />

b2b1dfcd7da9601d87905790852fa5aff95ebc32
Samiksha <singhsamikshasingh2004@gmail.com>
2024-04-28 14:18:12 +0530

    commiting login/signup design

--- src/components/samiksha/Login.js
+++ src/components/samiksha/Login.js
@@ -2,18 +2,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import a from './Login.module.css'; // Import your CSS module
+import loginImage from './loginimg.jpg'; // Import your login image
 
 const Login = () => {
   return (
     <div className={a.authContainer}>
+      <div className={a.authImage}>
+        <img src={loginImage} alt="Login" className={a.image} />
+      </div>
       <div className={a.authForm}>
-        <h2>Login</h2>
+        <h2 className={a.authTitle}>Login</h2>
         <form>
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-          <button type="submit">Login</button>
+          <input type="email" placeholder="Email" className={a.inputField} />
+          <input type="password" placeholder="Password" className={a.inputField} />
+          <button type="submit" className={a.submitButton}>Login</button>
         </form>
-        <p>New here? <Link to="/signup">Sign up now</Link></p>
+        <p className={a.switchAuth}>New here? <Link to="/signup" className={a.switchLink}>Sign up now</Link></p>
       </div>
     </div>
   );
--- src/components/samiksha/Signup.js
+++ src/components/samiksha/Signup.js
@@ -2,19 +2,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import a from './Signup.module.css'; // Import your CSS module
+import signupImage from './signupimage.jpg'; // Import your signup image
 
-const Signup = () => {
+const Signup= () => {
   return (
     <div className={a.authContainer}>
+      {/* <div className={a.authImage}>
+        <img src={signupImage} alt="Signup" className={a.image} />
+      </div> */}
       <div className={a.authForm}>
-        <h2>Sign Up</h2>
+        <h2 className={a.authTitle}>Sign Up</h2>
         <form>
-          <input type="text" placeholder="Username" />
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-          <button type="submit">Sign Up</button>
+          <input type="text" placeholder="Username" className={a.inputField} />
+          <input type="email" placeholder="Email" className={a.inputField} />
+          <input type="password" placeholder="Password" className={a.inputField} />
+          <button type="submit" className={a.submitButton}>Sign Up</button>
         </form>
-        <p>Already have an account? <Link to="/login">Login now</Link></p>
+        <p className={a.switchAuth}>Already have an account? <Link to="/login" className={a.switchLink}>Login now</Link></p>
+      </div>
+      <div className={a.authImage}>
+        <img src={signupImage} alt="Signup" className={a.image} />
       </div>
     </div>
   );

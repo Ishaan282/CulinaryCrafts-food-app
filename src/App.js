049cf4225d3b160eb2edf7bcf48696036aa73c25
Samiksha <singhsamikshasingh2004@gmail.com>
2024-04-29 17:06:26 +0530

    sanjal's commit

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-rou
 import Navbar from './components/Navbar';
 import Home from './components/samiksha/Home';
 import Cart from './components/sameer/Cart';
-import Recipes from './components/sanjal/Recipes';
+import Fruitcake from './components/sanjal/Fruitcake';
 import Social from './components/ishaan/Social';
 import Login from './components/samiksha/Login';
 import Signup from './components/samiksha/Signup';
+import Recipes from './components/sanjal/Recipes';
 
 function App() {
   return (
@@ -21,7 +22,8 @@ function App() {
           <Route path="/Recipes" element={<Recipes />} />
           <Route path="/Login" element={<Login />} />
           <Route path="/Signup" element={<Signup />} />
-
+          <Route path="/Signup" element={<Signup />} />
+          <Route path="/Fruitcake" element={<Fruitcake />} />
         </Routes>
       
     </BrowserRouter>

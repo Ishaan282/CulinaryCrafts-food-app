082e07adcca27c055320565ab43a3e20ee0fb9e3
Ishaan Singla <114952038+Ishaan1106@users.noreply.github.com>
2024-08-27 18:21:14 +0530

    hallo

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express(); //#express 
 const mongoose = require('mongoose'); //#mongoose
 
-//$ mongoose connection
+//connecting to server
 const dbURI = 'mongodb+srv://ishaan:water_bottle@cluster0.rdcay.mongodb.net/sandwitch?retryWrites=true&w=majority&appName=Cluster0';
 mongoose.connect(dbURI)
     .then((result) => {
@@ -17,5 +17,6 @@ mongoose.connect(dbURI)
 app.get('/',(req,res) => {
     res.send('Amaricaya Halo :D');
 })
+//DON'T YOU DARE EDIT THE ABOVE PORTION or else you will be cursed by Mango
 
-
+//!please start your code from here <3

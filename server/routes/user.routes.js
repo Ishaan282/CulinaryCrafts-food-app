7c321ce58b980f7a8f9fada888900454cd14a255
samiksha <singhsamikshasingh2004@gmail.com>
2024-11-28 15:24:05 +0530

    minor chnages

---
+++ server/routes/user.routes.js
@@ -0,0 +1,7 @@
+import {Router} from 'express';
+import { AuthController } from '../controllers/auth.controller';
+
+const router = Router();
+
+router.post('/signup',AuthController.signUp);
+router.post('/login', AuthController.login)

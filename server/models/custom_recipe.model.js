d7040fb6443c00f8ee1a9967d50deacdb44adb05
Ishaan282 <ishaansingla282@gmail.com>
2025-04-23 18:03:02 +0530

    made change in schema

--- server/models/custom_recipe.model.js
+++ server/models/custom_recipe.model.js
@@ -1,16 +1,18 @@
 const mongoose = require("mongoose");
 
 const ingredientsSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
     username: {
         type: String,
         required: true,
         trim: true
     },
+    
+    recipe_name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
     prompt: {
         type: String,
         required: false,
